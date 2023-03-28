44a84478eebc4b1bfd5ac6648ce635ac19bc65ff
Willi Zschiebsch <willi.zschiebsch@gmail.com>
2023-03-28 12:35:31 +0200

    updated capacity

--- quintus/evals/battery/capacity/eval.py
+++ quintus/evals/battery/capacity/eval.py
@@ -5,12 +5,12 @@ from .model import Electrode
 
 class CapacityEvaluation(BatteryEvaluation):
     def __init__(self):
-        super().__init__(Electrode, Electrode, Measurements, Measurements)
+        super().__init__(Electrode, Electrode, None, None)
 
     def evaluate_stack(
         self,
-        anode: Electrode,
-        cathode: Electrode,
+        anode: Measurements,
+        cathode: Measurements,
         foil: Measurements,
         separator: Measurements,
     ) -> float:
