9eea394dd6cd0f100124c76335a3d969955c39ab
Willi Zschiebsch <willi.zschiebsch@gmail.com>
2023-03-29 13:58:37 +0200

    implemented new filter system in optimizer

--- quintus/optimization/optimizer.py
+++ quintus/optimization/optimizer.py
@@ -1,16 +1,19 @@
 from quintus.io import DataSet, DataWriter
-from quintus.evals import Evaluation, collect_required_attr, generate_filters
+from quintus.evals import Evaluation, generate_filters
 
 
 class Optimizer:
     def __init__(
-        self, dataset: DataSet, evaluations: set[Evaluation], writer: DataWriter
+        self,
+        dataset: DataSet,
+        evaluations: set[Evaluation],
+        writer: DataWriter,
+        combined_search=True,
     ):
         self.evaluations = evaluations
         self.writer = writer
 
-        requirements = collect_required_attr(evaluations)
-        filters = generate_filters(requirements)
+        filters = generate_filters(evaluations)
         self.dataset = dataset
         self.subsets = dict[str, DataSet]()
 
