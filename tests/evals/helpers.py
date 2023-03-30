3f4a6e6e7c25f6e8a224d28218a787c302bab280
Willi Zschiebsch <willi.zschiebsch@gmail.com>
2023-03-30 14:55:23 +0200

    test fixes

--- tests/evals/helpers.py
+++ tests/evals/helpers.py
@@ -3,10 +3,7 @@ from quintus.evals.evaluation import BasicEvaluation
 
 class EvaluationTestAttrs(BasicEvaluation):
     def __init__(self, attrs: dict[str, dict]):
-        self.required_attrs = attrs
-
-    def filter_per_args(self) -> dict[str, dict]:
-        return self.required_attrs
+        super().__init__("TestAttrs", None, attrs)
 
     def compute(self, **kwargs) -> float:
         return 0.0
