625ce22a324a2a6b07f929c096ecc6696bc00622
Willi Zschiebsch <willi.zschiebsch@gmail.com>
2025-02-05 10:29:19 +0100

    added io

---
+++ quintus/src/io/traits.rs
@@ -0,0 +1,23 @@
+use crate::structures::Component;
+use std::vec::Vec;
+use std::result::Result;
+
+pub trait DataReader{
+    fn components(&self) -> Vec<Component>;
+}
+
+pub trait DataSet{
+    type Output: DataSet;
+    fn reduce(&self, filter: &str) -> Self::Output;
+    fn find(&self, filter: &str) ->  Result<Vec<Component>, impl std::error::Error>;
+}
+
+pub enum WriteModus{
+    Insert,
+    Update
+}
+pub trait DataWriter {
+    fn write_component(&mut self, component: &Component, modus: &WriteModus) -> Result<(), impl std::error::Error>; // -> Result<()>;
+    fn delete_component(&mut self, component: &Component);
+    fn clean_up(&self);
+}
